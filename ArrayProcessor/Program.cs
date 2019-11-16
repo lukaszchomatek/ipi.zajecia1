8510b6d88987c17f8f89da84b56033464e568d18
lukaszchomatek <lukasz.chomatek@p.lodz.pl>
2019-11-16 15:39:12 +0100

    nowy projekt

---
+++ ArrayProcessor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica = { 1, 3, 5, 6, 4, 8, 10 };
+
+            wypiszNajwiekszyElement(tablica);
+        }
+
+        private static void wypiszNajwiekszyElement(int[] tablica)
+        {
+
+        }
+    }
+}
